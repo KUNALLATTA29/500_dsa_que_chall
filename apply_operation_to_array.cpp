95f0699177f946f2663b864abc5728973f22592c
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-10-04 21:15:53 +0530

    Create apply_operation_to_array.cpp

---
+++ apply_operation_to_array.cpp
@@ -0,0 +1,23 @@
+vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1] = 0;
+            }
+        }
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+
+        for(int i=index;i<n;i++){
+            nums[i]=0;
+        }
+        
+
+        return nums;
+    }
