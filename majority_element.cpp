55f92f97536224309c60e82877dd7de2c58d790f
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-10-06 22:02:12 +0530

    Create majority_element.cpp

---
+++ majority_element.cpp
@@ -0,0 +1,14 @@
+int majorityElement(vector<int>& nums) {
+        map<int,int> map;
+
+        for(auto it:nums){
+            map[it]++;
+        }
+        for(auto it:map){
+            if(it.second > nums.size()/2){
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
