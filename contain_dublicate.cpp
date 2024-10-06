75ec605ba558e9f85b4b608cc9a1df6826386145
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-10-06 22:00:55 +0530

    Create contain_dublicate.cpp

---
+++ contain_dublicate.cpp
@@ -0,0 +1,13 @@
+bool containsDuplicate(vector<int>& nums) {
+        map<int,int> map;
+        bool ans = false;
+        for(auto it:nums){
+            map[it]++;
+        }
+        for(auto it:map){
+            if(it.second>=2){
+                ans=true;
+            }
+        }
+        return ans;
+    }
