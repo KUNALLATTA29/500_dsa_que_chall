c13a9ff4da0e40e8a713e2af5f339f0a7b055cae
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-10-08 21:07:41 +0530

    Create Find the Duplicate Number.cpp

---
+++ Find the Duplicate Number.cpp
@@ -0,0 +1,12 @@
+ int findDuplicate(vector<int>& nums) {
+         map<int,int> map;
+        for(auto it:nums){
+            map[it]++;
+        }
+        for(auto it:map){
+            if(it.second>=2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
