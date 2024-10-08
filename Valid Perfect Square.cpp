6748ca54dba8589cbb0c1476428e06632cd37784
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-10-08 21:04:53 +0530

    Create Valid Perfect Square.cpp

---
+++ Valid Perfect Square.cpp
@@ -0,0 +1,8 @@
+bool isPerfectSquare(int num) {
+        for(int i=1;i<=sqrt(num);i++){
+            if(i*i==num){
+                return true;
+            }
+        }
+        return false;
+    }
