f5718bc587a3f33cc467bc9345062fa6898a8531
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-10-08 21:06:14 +0530

    Create Sum of Square Numbers.cpp

---
+++ Sum of Square Numbers.cpp
@@ -0,0 +1,15 @@
+bool judgeSquareSum(int c) {
+        long long l=0,h=sqrt(c),m;
+        while(l<=h){
+            m=l*l + h*h;
+            if(m==c){
+                return true;
+            }else if(m<c){
+                l++;
+            }else if(m>c){
+                h--;
+            }
+        }
+        return false;
+
+    }
